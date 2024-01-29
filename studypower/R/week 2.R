c9b09dfacdc4711df95a242f7aab9b2c6f494b74
clementh626 <142791615+clementh626@users.noreply.github.com>
2024-01-28 22:43:19 -0800

    Delete studypower directory

--- studypower/R/week 2.R
+++
@@ -1,5 +0,0 @@
-sample_size <- function(var, significance, power, mde){
-  n=2*(var)*((qnorm(1-(significance/2))+qnorm(power))**2)/(mde**2)
-  return(ceiling(n))
-}
-print(sample_size(500**2, 0.05, 0.8, 100))
